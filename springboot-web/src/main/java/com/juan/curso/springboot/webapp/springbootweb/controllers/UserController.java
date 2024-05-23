6292de5178419046516e53211f696edfdcc970cb
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-05-22 22:07:37 -0500

    feat: :sparkles: Add constructors, getters and setters of email in user.java
    Too add a method with the list of users in the endpoint /list

--- springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.juan.curso.springboot.webapp.springbootweb.controllers;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.juan.curso.springboot.webapp.springbootweb.models.User;
 
@@ -13,8 +19,34 @@ public class UserController {
     public String details (Model model) {
 
         User user = new User("Juan Pablo", "Arango");
+        user.setEmail("andres@correo.com");
+
         model.addAttribute("title", "Hola mundo Spring Boot");
         model.addAttribute("user", user);
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        
+        // model.addAttribute("users", users);
+        model.addAttribute("tittle", "Listado de usuarios");
+
+        return "list";
+    }
+
+    // Nombre de atributo que se va a usar en la vista y se puede usar en cualquier metodo del controlador
+    // Sirve para no tener que estar pasando el objeto en cada metodo (reutilizarlo)
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        List<User> users = Arrays.asList(
+            new User("Pepa", "Gonzales"), 
+            new User("Lucas", "Gonzales", "lalo@correo.com"),
+            new User("Juan", "Gonzales", "juan@correo.com"),
+            new User("Andres", "Gonzales"));
+
+        return users;
+    }
+
+    
 }
--- springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/models/User.java
+++ springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/models/User.java
@@ -4,11 +4,20 @@ public class User {
 
     private String name;    
     private String lastname;
+    private String email;
     
-    //Click derecho, accion de codigo fuente --> Generar constructores 
+    //Click derecho, accion de codigo fuente --> Generar constructores
+    public User() {
+    }
+
     public User(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
+    }
+
+    public User(String name, String lastname, String email) {
+        this(name, lastname);
+        this.email = email;
     }
 
     public String getName() {
@@ -24,5 +33,13 @@ public class User {
         this.lastname = lastname;
     }
 
-    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+        
 }
