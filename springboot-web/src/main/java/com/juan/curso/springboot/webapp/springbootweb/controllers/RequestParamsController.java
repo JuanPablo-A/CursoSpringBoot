f8ee7a24da9900774923198f684b66d161919b9d
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-05-23 10:05:44 -0500

    feat: :sparkles: a
    Add new route with HttpServletRequest

--- springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
+++ springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.juan.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
 import com.juan.curso.springboot.webapp.springbootweb.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 /*
@@ -36,6 +37,26 @@ public class RequestParamsController {
         params.setMessage(text);
         params.setCode(code);
         return params;
+    }
+
+    /*
+        HttpServletRequest: proporciona métodos para obtener información sobre una solicitud HTTP.
+    */
+
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request) {
+        
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) { 
+        } 
+
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
+
     }
 
 }
