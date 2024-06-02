1d6675826efc09667011ba2df6dad9e4449df6e9
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-06-01 19:08:20 -0500

    feat: :sparkles: Add new methods using value Injection with @value

--- springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/juan/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -1,18 +1,32 @@
 package com.juan.curso.springboot.webapp.springbootweb.controllers;
 
 import java.util.HashMap;
-import java.util.Map; 
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juan.curso.springboot.webapp.springbootweb.models.User;
 import com.juan.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
 
     /*
         Path variable: se utiliza para vincular el valor de la variable de ruta a un parámetro de método de controlador.
@@ -36,4 +50,28 @@ public class PathVariableController {
 
         return json; 
     } 
+
+    /*
+      (@RequestBody User user) en el método create indica que el cuerpo de la 
+      solicitud HTTP debe ser convertido a un objeto User. 
+    */
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user ){
+
+        //Hacer algo con el usuario (save en la BD)
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values (@Value("${config.message}") String message) {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        return  json;
+    }
 }
